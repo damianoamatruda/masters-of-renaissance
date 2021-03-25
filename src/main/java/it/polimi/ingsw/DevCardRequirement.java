054a6c68f4a4c079d727b61cca28b283c09fdfb2
Marco Ciarrocca <marco.ciarrocca@mekomi.dev>
2021-03-25 13:55:08 +0100

    Create concrete classes for LeaderCard requirements

---
+++ src/main/java/it/polimi/ingsw/DevCardRequirement.java
@@ -0,0 +1,29 @@
+package it.polimi.ingsw;
+
+import java.util.Map;
+
+public class DevCardRequirement implements LeaderCardRequirement {
+  /**
+   * The development cards required to activate the leader card.
+   */
+  private final Map<DevelopmentCard, Integer> devCards;
+  
+  /**
+   * Class constructor.
+   * 
+   * @param devCard the development cards that form the requirement.
+   */
+  public DevCardRequirement(Map<DevelopmentCard, Integer> devCards) { this.devCards = devCards; }
+
+  /**
+   * Checks whether the player owns the development cards necessary to activate the leader card.
+   * 
+   * @param player  the player to be checked for development card ownership.
+   */
+  @Override
+  public void checkRequirements(Player player) {
+    // TODO Auto-generated method stub
+    
+  }
+  
+}
---
+++ src/main/java/it/polimi/ingsw/ResourceRequirement.java
@@ -0,0 +1,38 @@
+package it.polimi.ingsw;
+
+import java.util.Map;
+
+/**
+ * A concrete requirement for leader card activation.
+ * 
+ * A <code>LeaderCard</code> with a <code>ResourceRequirement</code> can only be activated
+ * if the <code>Player</code> that owns it also owns the specified amount of <code>Resource</code>s.
+ * 
+ * @see LeaderCard
+ * @see LeaderCardRequirement
+ * @see Player
+ */
+public class ResourceRequirement implements LeaderCardRequirement {
+  /**
+   * The the resources required to activate the leader card.
+   */
+  private final Map<ResourceType, Integer> resources;
+
+  /**
+   * Class constructor.
+   * 
+   * @param resources the resources that form the requirement.
+   */
+  public ResourceRequirement(Map<ResourceType, Integer> resources) { this.resources = resources; }
+
+  /**
+   * Checks whether the player owns the resources necessary to activate the leader card.
+   * 
+   * @param player  the player to be checked for resource ownership.
+   */
+  @Override
+  public void checkRequirements(Player player) {
+    // TODO Auto-generated method stub
+    
+  }
+}
