117ac4be0d3eaa9cc0bdc4469b709ee5bdb2e2d1
Marco Ciarrocca <marco.ciarrocca@mekomi.dev>
2021-03-25 15:45:30 +0100

    Create skeleton for resourceType

--- src/main/java/it/polimi/ingsw/ResourceType.java
+++ src/main/java/it/polimi/ingsw/ResourceType.java
@@ -1,4 +1,31 @@
 package it.polimi.ingsw;
 
-public class ResourceType {
+public abstract class ResourceType {
+  /**
+   * Class constructor.
+   */
+  private ResourceType() { }
+
+  /**
+   * @return the single instance of this class.
+   */
+  public static ResourceType getInstance() { return null; }
+
+  /**
+   * @return  the name of the resource associated with the class.
+   *          For UI purposes only.
+   */
+  public abstract String getName();
+
+  /**
+   * @return  whether the resource can be replaced with another resource.
+   */
+  public boolean isBlank() { return false; }
+
+  /**
+   * Routine for consuming the resource. Its effect is adding the resource to the player.
+   * 
+   * @param player  the player the resource goes to.
+   */
+  public abstract void onTaken(Player player);
 }
