dc1d98c2090a471ef5bfc3a03ffbc2e2fa1954c7
Marco Ciarrocca <marco.ciarrocca@mekomi.dev>
2021-03-25 13:54:03 +0100

    Create Card interface

---
+++ src/main/java/it/polimi/ingsw/Card.java
@@ -0,0 +1,29 @@
+package it.polimi.ingsw;
+
+/**
+ * Card is the abstract class for all card-like entities
+ * that share the victory points attribute.
+ * 
+ * @see DevelopmentCard
+ * @see LeaderCard
+ */
+public abstract class Card {
+  /**
+   * The amount of points the card gives to its owner.
+   */
+  private final int victoryPoints; //TODO should be protected?
+
+  /**
+   * Class constructor.
+   * 
+   * @param victoryPoints the amount of victory points associated with the card.
+   * @see Game
+   * @see Player
+   */
+  public Card(int victoryPoints) { this.victoryPoints = victoryPoints; }
+
+  /**
+   * @return  the amount of victory points set when created.
+   */
+  public int getVictoryPoints() { return victoryPoints; }
+}
