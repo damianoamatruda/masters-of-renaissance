c5d200f5e035007affb432c5c66c9a9cacc0960e
Damiano Amatruda <damiano.amatruda@outlook.com>
2021-03-25 04:10:11 +0100

    Reapply reverted changes

--- src/main/java/it/polimi/ingsw/Player.java
+++ src/main/java/it/polimi/ingsw/Player.java
@@ -10,41 +10,54 @@ import java.util.Stack;
 public class Player {
     /** Number of possible production slots that can be occupied by development cards */
     private static final int DEV_SLOTS_COUNT=3;
+
     /** Number of the last reachable faith track tile */
     private static final int MAX_FAITH_POINTS_COUNT=24;
+
     /** Number of basic shelves inside of the player's warehouse */
     private static final int WAREHOUSE_SHELVES_COUNT=3;
 
     /** The current game instance in which the player is playing */
     private Game game;
+
     /** The player's nickname */
     private String nickname;
+
     /** The hand of leader cards available to the player */
     private List<LeaderCard> leaders;
+
     /** The player's warehouse, standard container of buyable resources */
     private List<Shelf> warehouse;
+
     /** The player's strongbox, where all the production output goes */
     private Strongbox strongbox;
+
     /** The collection of cards in each production slot */
     private List<Stack<DevelopmentCard>> devSlots;
+
     /** The base production "recipe" */
     private Production baseProduction;
+
     /** Visible to the view, this indicates whether the player starts first each turn */
     private boolean inkwell;
+
     /** The player's faith track marker */
     private int faithPoints;
+
     /** The player's score */
     private int victoryPoints;
+
     /** The player's state of connection to the game */
     private boolean active;
+
     /** The flag to be set when a winner has been decided */
     private boolean winner;
 
     /** Initializes player's attributes
-     * @param game the game to which the player is connected
-     * @param nickname the player's nickname to be seen by all the players
-     * @param leaders the 4 initially assigned leader cards
-     * @param inkwell received only by the first player
+     * @param game      the game to which the player is connected
+     * @param nickname  the player's nickname to be seen by all the players
+     * @param leaders   the 4 initially assigned leader cards
+     * @param inkwell   received only by the first player
      */
     public Player(Game game, String nickname, List<LeaderCard> leaders, boolean inkwell){
         this.game=game;
@@ -56,34 +69,40 @@ public class Player {
         active=true;
         winner=false;
     }
+
     /** Getter of the number of production slots available
      * @return the number of slots */
     public static int getDevSlotsCount(){
         return DEV_SLOTS_COUNT;
     }
+
     /** Getter of the number of the last reachable tile
      * @return the last reachable tile */
     public static int getMaxFaithPointsCount(){
         return MAX_FAITH_POINTS_COUNT;
     }
+
     /** Getter of the number of basic shelves of which the warehouse is composed
      * @return the number of basic shelves (leader depots excluded) */
     public static int getWarehouseShelvesCount(){
         return WAREHOUSE_SHELVES_COUNT;
     }
+
     /** Getter of the player's visible nickname
      * @return the player's nickname */
     public String getNickname(){
         return nickname;
     }
+
     /** Getter of the remaining amount of leader cards that are, or can be, activated
      * @return the left amount of leader cards that are accessible to the player */
     public int getLeadersCount(){
         return leaders.size();
     }
+
     /** Getter of the leader card at the corresponding index
      * @param index the index that points the card to be retrieved
-     * @return the required leader card */
+     * @return      the required leader card */
     public LeaderCard getLeader(int index){
         return leaders.get(index);
     }
@@ -116,7 +135,7 @@ public class Player {
     /**
      * Retrieves the required shelf in order to perform further actions on it
      * @param index the number of shelf to be retrieved
-     * @return the required shelf
+     * @return      the required shelf
      */
     public Shelf getShelf(int index){
         return warehouse.get(index);
@@ -133,17 +152,15 @@ public class Player {
     /**
      * Retrieves the stack of cards bought and stored in a given slot
      * @param index the number of slot to be accessed
-     * @return the cards located in the required slot
+     * @return      the cards located in the required slot
      */
     public Stack<DevelopmentCard> getDevSlot(int index){
         return devSlots.get(index);
     }
-    //
-    //
 
     /**
      * Checks whether the player has the inkwell, i.e. the player starts first at each turn
-     * @return true if this player starts first
+     * @return  true if this player starts first
      */
     public boolean hasInkwell(){
         return inkwell;
@@ -151,7 +168,7 @@ public class Player {
 
     /**
      * Gets the position of the player's faith marker
-     * @return The number of tile reached by the player
+     * @return  The number of tile reached by the player
      */
     public int getFaithPoints() {
         return faithPoints;
@@ -159,7 +176,7 @@ public class Player {
 
     /**
      * Says how many victory points have been scored so far by the player
-     * @return current player score
+     * @return  current player score
      */
     public int getVictoryPoints() {
         return victoryPoints;
@@ -167,7 +184,7 @@ public class Player {
 
     /**
      * Increments score of the player by a given quantity
-     * @param points the quantity to be added to the score
+     * @param points    the quantity to be added to the score
      */
     public void incrementVictoryPoints(int points) {
         this.victoryPoints += victoryPoints;
@@ -175,7 +192,7 @@ public class Player {
 
     /**
      * Checks whether the player is connected to the current game
-     * @return true if player is online
+     * @return  true if player is online
      */
     public boolean isActive() {
         return active;
@@ -183,7 +200,7 @@ public class Player {
 
     /**
      * Changes the state of connection of a player, in case of (dis)connection
-     * @param active the updated state of connection of the player
+     * @param   active the updated state of connection of the player
      */
     public void setActive(boolean active) {
         this.active = active;
@@ -191,41 +208,41 @@ public class Player {
 
     /**
      * Says whether this player has won the game or not
-     * @return true if this player is winner of the game
+     * @return  true if this player is winner of the game
      */
     public boolean isWinner() {
         return winner;
     }
 
     /** Declares the winner, by setting the winner flag
-     * @param winner true if player is to be declared as winner
-     * */
+     * @param winner    true if player is to be declared as winner
+     */
     public void setWinner(boolean winner) {
         this.winner = winner;
     }
 
     /** Decides whether a given leader card can still be discarded
      * @param index number of the leader card to be checked
-     * @return true if the leader card can be discarded during the current turn
-     * */
+     * @return      true if the leader card can be discarded during the current turn
+     */
     private boolean canDiscardLeader(int index){
         return !getLeader(index).isActive();
     }
 
     /** Decides whether an available development card can be deposited on top of a given production slot
-     * @param index the number of slot to be checked
-     * @param devCard the development card that the user wishes to buy
-     * @return true if the current top card of the slot is of the previous level, thus the action can be done
-     * */
+     * @param index     the number of slot to be checked
+     * @param devCard   the development card that the user wishes to buy
+     * @return          true if the current top card of the slot is of the previous level, thus the action can be done
+     */
     private boolean canAddToDevSlot(int index, DevelopmentCard devCard){
         return getDevSlot(index).peek().getLevel() < 3;
     }
 
     /**
      * Places a new card on top of a given production slot
-     * @param index the destination production slot
-     * @param devCard the development card that has just been bought
-     * @throws Exception blocks the action if the level of the previous top card of the slot is not equal to current level minus 1
+     * @param index         the destination production slot
+     * @param devCard       the development card that has just been bought
+     * @throws Exception    blocks the action if the level of the previous top card of the slot is not equal to current level minus 1
      */
     public void addToDevSlot(int index, DevelopmentCard devCard) throws Exception {
         Stack<DevelopmentCard> slot = devSlots.get(index);
@@ -240,5 +257,4 @@ public class Player {
     public Production getBaseProduction(){
         return baseProduction;
     }
-
 }
