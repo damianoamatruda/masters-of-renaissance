f7b2754251dca045b1e02d3f6ca128b196160092
Marco Ciarrocca <marco.ciarrocca@mekomi.dev>
2021-03-25 15:45:15 +0100

    Create skeleton for DevelopmentCard

--- src/main/java/it/polimi/ingsw/DevelopmentCard.java
+++ src/main/java/it/polimi/ingsw/DevelopmentCard.java
@@ -1,9 +1,54 @@
 package it.polimi.ingsw;
 
-public class DevelopmentCard {
-    private int level;
+import java.util.Map;
 
-    public int getLevel() {
-        return level;
-    }
+public class DevelopmentCard extends Card {
+  private final DevCardColor color;
+  private final Map<ResourceType, Integer> cost;
+  private final int level;
+  private final Production production;
+
+  /**
+   * Class constructor.
+   * 
+   * @param color         the color of the card.
+   * @param level         the level of the card.
+   * @param cost          the resources needed to buy the card.
+   * @param production    the production associated with the card.
+   * @param victoryPoints the victory points associated with the card.
+   */
+  public DevelopmentCard(DevCardColor color, int level, Map<ResourceType, Integer> cost, Production production, int victoryPoints) {
+    super(victoryPoints);
+    this.color = color;
+    this.cost = cost;
+    this.level = level;
+    this.production = production;
+  }
+
+  /**
+   * @return  the card's color.
+   */
+  public DevCardColor getColor() { return color; }
+  
+  /**
+   * @return  the card's cost.
+   */
+  public Map<ResourceType, Integer> getCost() { return cost; }
+  
+  /**
+   * @return  the card's level.
+   */
+  public int getLevel() { return level; }
+  
+  /**
+   * @return  the card's production.
+   */
+  public Production getProduction() { return production; }
+  
+  /**
+   * @param player the player taking the card.
+   */
+  public void onTaken(Player player) {
+    //TODO implement
+  }
 }
