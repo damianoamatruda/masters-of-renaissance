26a0b5c81e0f0cf1664f9d9e3f75677b08e1582d
Damiano Amatruda <damiano.amatruda@outlook.com>
2021-03-26 19:48:59 +0100

    Add test classes for devcardcolors Blue and Yellow

---
+++ src/test/java/it/polimi/ingsw/devcardcolors/BlueTest.java
@@ -0,0 +1,15 @@
+package it.polimi.ingsw.devcardcolors;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Test of properties of color Green.
+ */
+public class BlueTest {
+    /** Check name. */
+    @Test
+    public void getName(){
+        assertEquals("Blue", new Blue().getName());
+    }
+}
