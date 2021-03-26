365e071b346e53a83dcad743924dd66eea1f1db1
Marco Ciarrocca <marco.ciarrocca@mekomi.dev>
2021-03-26 02:59:10 +0100

    Create ZeroLeader class

---
+++ src/main/java/it/polimi/ingsw/ZeroLeader.java
@@ -0,0 +1,21 @@
+package it.polimi.ingsw;
+
+import java.util.Map;
+
+public class ZeroLeader extends LeaderCard {
+  /**
+   * Class constructor.
+   * 
+   * @param resource      the resource bound to the card.
+   *                      The card's ability is restricted to acting on this resource type only.
+   * @param requirement   the requirement to be satisfied for card activation.
+   * @param victoryPoints the victory points associated with the card.
+   */
+  public ZeroLeader(ResourceType resource, LeaderCardRequirement requirement, int victoryPoints) {
+    super(resource, requirement, victoryPoints);
+  }
+
+  //TODO implement
+  @Override
+  public Map<ResourceType, Integer> processZeros(Map<ResourceType, Integer> resources, Map<ResourceType, Integer> zeros) { return resources; }
+}
