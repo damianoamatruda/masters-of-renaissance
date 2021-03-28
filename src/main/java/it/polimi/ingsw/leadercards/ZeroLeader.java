378eae76631a785478b3477639b0702f5bec4df8
Marco Ciarrocca <marco.ciarrocca@mekomi.dev>
2021-03-28 01:36:28 +0100

    Implement ZeroLeader.processZeros

--- src/main/java/it/polimi/ingsw/leadercards/ZeroLeader.java
+++ src/main/java/it/polimi/ingsw/leadercards/ZeroLeader.java
@@ -2,6 +2,7 @@ package it.polimi.ingsw.leadercards;
 
 import it.polimi.ingsw.CardRequirement;
 import it.polimi.ingsw.resourcetypes.ResourceType;
+import it.polimi.ingsw.resourcetypes.Zero;
 
 import java.util.Map;
 
@@ -24,7 +25,21 @@ public class ZeroLeader extends LeaderCard {
 
     @Override
     public Map<ResourceType, Integer> processZeros(Map<ResourceType, Integer> resources, Map<ResourceType, Integer> zeros) {
-        // TODO: Implement
-        return resources;
+        Map<ResourceType, Integer> resCopy = Map.copyOf(resources);
+        
+        // if zeros contains this card's resource -> card has to be activated (leader was chosen by player)
+        if (zeros.containsKey(this.getResource())) {
+            // take amount of resources of this type to be substituted from zeros map
+            // and add them to the output resources
+            resCopy.compute(this.getResource(), (res, amount) -> amount + zeros.get(res));
+            
+            // subtract amount of substituted resources from zero key
+            resCopy.computeIfPresent(Zero.getInstance(), (res, amount) -> amount - zeros.get(res));
+            
+            // delete entry so it can't be re-processed when called in cascade with other leaders
+            zeros.remove(this.getResource());
+        }
+        
+        return resCopy;
     }
 }
