2a62683b757c9c26fda22182e28a035a4e45c719
Marco Ciarrocca <marco.ciarrocca@mekomi.dev>
2021-03-29 19:38:30 +0200

    Implement tests for DepotLeader

--- src/test/java/it/polimi/ingsw/leadercards/DepotLeaderTest.java
+++ src/test/java/it/polimi/ingsw/leadercards/DepotLeaderTest.java
@@ -1,10 +1,43 @@
 package it.polimi.ingsw.leadercards;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.Test;
+
+import it.polimi.ingsw.resourcetypes.Coin;
+
 
 /**
  * Test for the DepotLeader class.
  */
 public class DepotLeaderTest {
-    // TODO: Implement
+    /**
+     * Creation of a DepotLeader with its binding resource set to null.
+     */
+    @Test
+    void getNullResourceDepot() {
+        DepotLeader leader = new DepotLeader(1, null, null, 0);
+
+        assertNotNull(leader.getDepot());
+    }
+
+    /**
+     * Creation of a DepotLeader with a zero-sized depot.
+     */
+    @Test
+    void getZeroSizeDepot() {
+        DepotLeader leader = new DepotLeader(0, Coin.getInstance(), null, 0);
+
+        assertNotNull(leader.getDepot());
+    }
+
+    /**
+     * Creation of a DepotLeader with 'expected' parameters.
+     */
+    @Test
+    void getDepot() {
+        DepotLeader leader = new DepotLeader(1, Coin.getInstance(), null, 0);
+
+        assertNotNull(leader.getDepot());
+    }
 }
