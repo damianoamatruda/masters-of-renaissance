b8a1ed2c45173c6f59615e0836538ae9b219fad1
Marco Ciarrocca <marco.ciarrocca@mekomi.dev>
2021-03-29 19:56:06 +0200

    Implement tests for ProductionLeader

--- src/test/java/it/polimi/ingsw/leadercards/ProductionLeaderTest.java
+++ src/test/java/it/polimi/ingsw/leadercards/ProductionLeaderTest.java
@@ -1,10 +1,28 @@
 package it.polimi.ingsw.leadercards;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
 import org.junit.jupiter.api.Test;
+
+import it.polimi.ingsw.Production;
 
 /**
  * Test of properties of ProductionLeader
  */
 public class ProductionLeaderTest {
-    // TODO: Implement
+    @Test
+    void nullProduction() {
+        ProductionLeader leader = new ProductionLeader(null, null, null, 0);
+
+        assertNull(leader.getProduction());
+    }
+
+    @Test
+    void production() {
+        Production p = new Production(null, null);
+        ProductionLeader leader = new ProductionLeader(p, null, null, 0);
+
+        assertEquals(p, leader.getProduction());
+    }
 }
