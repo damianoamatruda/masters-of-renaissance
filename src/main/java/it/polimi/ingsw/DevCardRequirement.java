0d7d7a56ef4312564ece71985209e2c3087903e5
Marco Ciarrocca <marco.ciarrocca@mekomi.dev>
2021-04-03 13:40:10 +0200

    Fix type mismatch in DevCardRequirement constructor argument

--- src/main/java/it/polimi/ingsw/DevCardRequirement.java
+++ src/main/java/it/polimi/ingsw/DevCardRequirement.java
@@ -1,6 +1,5 @@
 package it.polimi.ingsw;
 
-import java.lang.reflect.Array;
 import java.util.*;
 
 import it.polimi.ingsw.devcardcolors.DevCardColor;
@@ -13,57 +12,74 @@ public class DevCardRequirement implements CardRequirement {
     /**
      * The development cards required to activate the leader card.
      */
-    private final Map<DevCardColor, int[]> reqCards;
+    private final List<DevCardRequirementEntry> entryList;
 
     /**
      * Class constructor.
      *
-     * @param devCards the development cards that form the requirement.
+     * @param requirements the development cards that form the requirement.
      */
-    public DevCardRequirement(List<DevelopmentCard> devCards) {
-        reqCards = new HashMap<>();
-
-        buildRequirements(devCards, reqCards);
+    public DevCardRequirement(Map<Map<DevCardColor, Integer>, Integer> requirements) {
+        entryList = buildRequirements(requirements);
     }
 
-    private void buildRequirements(List<DevelopmentCard> cards, Map<DevCardColor, int[]> requirements) {
-        cards.forEach(devCard ->
-            requirements.compute(devCard.getColor(), (color, levels) -> {
-                if (levels == null) levels = new int[Game.getDevGridLevelsCount()];
+    private List<DevCardRequirementEntry> buildRequirements(Map<Map<DevCardColor, Integer>, Integer> reqs) {
+        List<DevCardRequirementEntry> entries = new ArrayList<>();
 
-                levels[devCard.getLevel()]++;
-
-                return levels;
-            })
-        );
+        reqs.forEach((map, amount) -> map.forEach((color, level) -> entries.add(new DevCardRequirementEntry(color, level, amount))));
+        
+        return entries;
     }
 
     @Override
     public void checkRequirements(Player player) throws Exception {
-        Map<DevCardColor, int[]> playerCards = new HashMap<>();
+        List<DevelopmentCard> playerCards = new ArrayList<>();
 
-        // build map of player's cards for easy comparison
-        // using the same data structure as the requirements
         for (int i = 0; i < Player.getDevSlotsCount(); i++)
-            buildRequirements(player.getDevSlot(i), playerCards);
+            playerCards.addAll(player.getDevSlot(i));
         
-        // if the playercards don't contain all colors required
-        if (!playerCards.keySet().containsAll(reqCards.keySet())) throw new Exception();
-        
-        // compare each key's levels
-        // I would have done it this way, but Java doesn't want me to do things inside forEach
-        // reqCards.forEach((color, levels) -> {
-        //     if (Arrays.compare(playerCards.get(color), levels) < 0) throw new Exception();
-        // });
-        // so, here's the dumb way
-        ArrayList<DevCardColor> playerColors = new ArrayList<>(playerCards.keySet());
+        List<DevCardRequirementEntry>   playerState = new ArrayList<>(),
+                                        reqCopy = new ArrayList<>(entryList);
 
-        for (int i = 0; i < playerColors.size(); i++) {
-            int[] playerLevels = playerCards.get(playerColors.get(i)),
-                  reqLevels = reqCards.get(playerColors.get(i));
-            
-            if (Arrays.compare(playerLevels, reqLevels) < 0) throw new Exception();
+        playerCards.forEach(card -> {
+            int index = playerState.indexOf(new DevCardRequirementEntry(card.getColor(), card.getLevel()));
+            // if not present add a new entry
+            if (index < 0) playerState.add(new DevCardRequirementEntry(card.getColor(), card.getLevel(), 1));
+            // else increment the amount available
+            else  playerState.add(index, playerState.get(index).setAmount(playerState.get(index).amount + 1));
+        });
+
+        for (DevCardRequirementEntry entry : reqCopy) {
+            int index = playerState.indexOf(entry);
+            if (index < 0) throw new Exception(); // entry not found in player's cards -> requirements not satisfied
+
+            entry.setAmount(entry.amount - playerState.get(index).amount);
         }
+
+        // if the req hold positive amounts it means they haven't been satisfied
+        if (reqCopy.stream().filter(entry -> entry.amount > 0).count() > 0) throw new Exception();
     }
 
+    private class DevCardRequirementEntry {
+        public final DevCardColor color;
+        public final int level;
+        public int amount = 0;
+    
+        DevCardRequirementEntry(DevCardColor color, int level) {
+            this.color = color; this.level = level;
+        }
+        DevCardRequirementEntry(DevCardColor color, int level, int amount) {
+            this.color = color; this.level = level; this.amount = amount; 
+        }
+        
+        public DevCardRequirementEntry setAmount(int newAmount) { amount = newAmount; return this; }
+    
+        @Override
+        public boolean equals(Object o) {
+            if (!(o instanceof DevCardRequirementEntry)) return false;
+    
+            return  ((DevCardRequirementEntry)o).color == this.color &&
+                    ((DevCardRequirementEntry)o).level == this.level;
+        }
+    }
 }
