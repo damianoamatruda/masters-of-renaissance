f6150382deea1d2906287cb729d55d1b1a3249e9
Damiano Amatruda <damiano.amatruda@outlook.com>
2021-04-09 18:58:56 +0200

    Make 'WarehouseShelf' a static nested class

--- src/main/java/it/polimi/ingsw/model/resourcecontainers/Warehouse.java
+++ src/main/java/it/polimi/ingsw/model/resourcecontainers/Warehouse.java
@@ -1,5 +1,7 @@
 package it.polimi.ingsw.model.resourcecontainers;
 
+import it.polimi.ingsw.model.resourcetypes.ResourceType;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -50,5 +52,51 @@ public class Warehouse {
      */
     public List<WarehouseShelf> getShelves() {
         return shelves;
+    }
+
+    /**
+     * This class represents a shelf in a warehouse, that is a shelf that cannot contain a resource that is contained also
+     * in another shelf of the same warehouse.
+     *
+     * @see Warehouse
+     */
+    public static class WarehouseShelf extends Shelf {
+        /** The warehouse containing the shelf. */
+        private final Warehouse warehouse;
+
+        /**
+         * Initializes the shelf by specifying the warehouse containing it.
+         *
+         * @param warehouse the warehouse containing the shelf
+         * @param size      the maximum number of resources in the shelf
+         */
+        public WarehouseShelf(Warehouse warehouse, int size) {
+            super(size);
+            this.warehouse = warehouse;
+        }
+
+        /**
+         * Copy constructor. Makes a deep copy of a WarehouseShelf.
+         *
+         * @param warehouseShelf    the WarehouseShelf to copy
+         */
+        public WarehouseShelf(WarehouseShelf warehouseShelf) {
+            super(warehouseShelf);
+            warehouse = warehouseShelf.warehouse;
+        }
+
+        @Override
+        public ResourceContainer copy() {
+            return new WarehouseShelf(this);
+        }
+
+        @Override
+        public void addResource(ResourceType resType) throws Exception {
+            if (warehouse.shelves.stream()
+                    .filter(s -> !s.equals(this) && !s.isEmpty())
+                    .anyMatch(s -> s.getResType().equals(resType)))
+                throw new Exception();
+            super.addResource(resType);
+        }
     }
 }
