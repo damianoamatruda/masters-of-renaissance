5a6c7d5587e6a3a91fdd0b9c2dc577f67edaa3c9
Marco Ciarrocca <marco.ciarrocca@mekomi.dev>
2021-04-09 17:03:54 +0200

    Refactor DevCardRequirementEntry into a public class

--- src/main/java/it/polimi/ingsw/model/DevCardRequirement.java
+++ src/main/java/it/polimi/ingsw/model/DevCardRequirement.java
@@ -58,31 +58,4 @@ public class DevCardRequirement implements CardRequirement {
         // if the req hold positive amounts it means they haven't been satisfied
         if (reqCopy.stream().filter(entry -> entry.amount > 0).count() > 0) throw new Exception();
     }
-
-    // TODO: Add Javadoc
-    private class DevCardRequirementEntry {
-        public final DevCardColor color;
-        public final int level;
-        public int amount = 0;
-
-        // TODO: Add Javadoc
-        DevCardRequirementEntry(DevCardColor color, int level) {
-            this.color = color; this.level = level;
-        }
-        // TODO: Add Javadoc
-        DevCardRequirementEntry(DevCardColor color, int level, int amount) {
-            this.color = color; this.level = level; this.amount = amount; 
-        }
-
-        // TODO: Add Javadoc
-        public DevCardRequirementEntry setAmount(int newAmount) { amount = newAmount; return this; }
-    
-        @Override
-        public boolean equals(Object o) {
-            if (!(o instanceof DevCardRequirementEntry)) return false;
-    
-            return  ((DevCardRequirementEntry)o).color == this.color &&
-                    (((DevCardRequirementEntry)o).level == 0 || this.level == 0 || ((DevCardRequirementEntry)o).level == this.level);
-        }
-    }
 }
