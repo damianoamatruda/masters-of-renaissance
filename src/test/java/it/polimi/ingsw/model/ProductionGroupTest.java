b65ed86bd9544920ba6bf6a4f7de711ae02f3772
Damiano Amatruda <damiano.amatruda@outlook.com>
2021-04-17 19:58:15 +0200

    Refine tests of Production and ProductionGroup
    * Renames 'ProductionTest' to 'ProductionGroupTest'

--- src/test/java/it/polimi/ingsw/model/ProductionGroupTest.java
+++ src/test/java/it/polimi/ingsw/model/ProductionGroupTest.java
@@ -13,26 +13,37 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
- * Test class for Production.
+ * Test class for ProductionGroup.
  */
-public class ProductionTest {
+public class ProductionGroupTest {
     @Test
-    void productionWithoutBlanks() throws IllegalProductionActivationException {
+    void singleProductionWithoutBlanks() throws IllegalProductionActivationException {
         ResourceType r1 = new ResourceType("r1", true);
         ResourceType r2 = new ResourceType("r2", true);
-        Production production = new Production(Map.of(r1, 2), 0, Set.of(), Map.of(r2, 3), 0, Set.of(), false);
-
         Player player = new Player("", false, List.of(), new Warehouse(0), new Strongbox(), new Production(Map.of(), 0, Map.of(), 0), 0, 0, 0);
         Game game = new Game(List.of(player), new DevCardGrid(List.of(), 0, 0), null, new FaithTrack(Set.of(), Set.of()), 0, 0);
+        Production prod = new Production(
+                Map.of(r1, 2),
+                0,
+                Set.of(),
+                Map.of(r2, 3),
+                0,
+                Set.of(),
+                false
+        );
 
         for (int i = 0; i < 6; i++)
             player.getStrongbox().addResource(r1);
 
-        new ProductionGroup(List.of(
-                new ProductionGroup.ProductionRequest(production, Map.of(), Map.of(),
-                        Map.of(player.getStrongbox(), Map.of(r1, 2)),
-                        Map.of(player.getStrongbox(), Map.of(r2, 3)))
-        )).activate(game, player);
+        ProductionGroup.ProductionRequest pr1 = new ProductionGroup.ProductionRequest(
+                prod,
+                Map.of(),
+                Map.of(),
+                Map.of(player.getStrongbox(), Map.of(r1, 2)),
+                Map.of(player.getStrongbox(), Map.of(r2, 3))
+        );
+        ProductionGroup productionGroup = new ProductionGroup(List.of(pr1));
+        productionGroup.activate(game, player);
 
         assertAll("getResourceQuantity",
                 () -> assertEquals(4, player.getStrongbox().getResourceQuantity(r1)),
@@ -41,25 +52,28 @@ public class ProductionTest {
     }
 
     @Test
-    void productionWithInputBlanks() throws IllegalProductionActivationException {
+    void singleProductionWithInputBlanks() throws IllegalProductionActivationException {
         ResourceType r1 = new ResourceType("r1", true);
         ResourceType r2 = new ResourceType("r2", true);
         ResourceType r3 = new ResourceType("r3", true);
-        Production production = new Production(Map.of(r1, 2), 3, Set.of(), Map.of(r2, 3), 0, Set.of(), false);
-
         Player player = new Player("", false, List.of(), new Warehouse(0), new Strongbox(), new Production(Map.of(), 0, Map.of(), 0), 0, 0, 0);
         Game game = new Game(List.of(player), new DevCardGrid(List.of(), 0, 0), null, new FaithTrack(Set.of(), Set.of()), 0, 0);
+        Production prod = new Production(Map.of(r1, 2), 3, Set.of(), Map.of(r2, 3), 0, Set.of(), false);
 
         for (int i = 0; i < 6; i++)
             player.getStrongbox().addResource(r1);
         for (int i = 0; i < 2; i++)
             player.getStrongbox().addResource(r3);
 
-        new ProductionGroup(List.of(
-                new ProductionGroup.ProductionRequest(production, Map.of(r1, 1, r3, 2), Map.of(),
-                        Map.of(player.getStrongbox(), Map.of(r1, 3, r3, 2)),
-                        Map.of(player.getStrongbox(), Map.of(r2, 3)))
-        )).activate(game, player);
+        ProductionGroup.ProductionRequest productionRequest = new ProductionGroup.ProductionRequest(
+                prod,
+                Map.of(r1, 1, r3, 2),
+                Map.of(),
+                Map.of(player.getStrongbox(), Map.of(r1, 3, r3, 2)),
+                Map.of(player.getStrongbox(), Map.of(r2, 3))
+        );
+        ProductionGroup productionGroup = new ProductionGroup(List.of(productionRequest));
+        productionGroup.activate(game, player);
 
         assertAll("getResourceQuantity",
                 () -> assertEquals(3, player.getStrongbox().getResourceQuantity(r1)),
@@ -68,23 +82,26 @@ public class ProductionTest {
     }
 
     @Test
-    void productionWithOutputBlanks() throws IllegalProductionActivationException {
+    void singleProductionWithOutputBlanks() throws IllegalProductionActivationException {
         ResourceType r1 = new ResourceType("r1", true);
         ResourceType r2 = new ResourceType("r2", true);
         ResourceType r3 = new ResourceType("r3", true);
-        Production production = new Production(Map.of(r1, 2), 0, Set.of(), Map.of(r2, 3), 3, Set.of(), false);
-
         Player player = new Player("", false, List.of(), new Warehouse(0), new Strongbox(), new Production(Map.of(), 0, Map.of(), 0), 0, 0, 0);
         Game game = new Game(List.of(player), new DevCardGrid(List.of(), 0, 0), null, new FaithTrack(Set.of(), Set.of()), 0, 0);
+        Production prod = new Production(Map.of(r1, 2), 0, Set.of(), Map.of(r2, 3), 3, Set.of(), false);
 
         for (int i = 0; i < 6; i++)
             player.getStrongbox().addResource(r1);
 
-        new ProductionGroup(List.of(
-                new ProductionGroup.ProductionRequest(production, Map.of(), Map.of(r2, 2, r3, 1),
-                        Map.of(player.getStrongbox(), Map.of(r1, 2)),
-                        Map.of(player.getStrongbox(), Map.of(r2, 5, r3, 1)))
-        )).activate(game, player);
+        ProductionGroup.ProductionRequest productionRequest = new ProductionGroup.ProductionRequest(
+                prod,
+                Map.of(),
+                Map.of(r2, 2, r3, 1),
+                Map.of(player.getStrongbox(), Map.of(r1, 2)),
+                Map.of(player.getStrongbox(), Map.of(r2, 5, r3, 1))
+        );
+        ProductionGroup productionGroup = new ProductionGroup(List.of(productionRequest));
+        productionGroup.activate(game, player);
 
         assertAll("getResourceQuantity",
                 () -> assertEquals(4, player.getStrongbox().getResourceQuantity(r1)),
