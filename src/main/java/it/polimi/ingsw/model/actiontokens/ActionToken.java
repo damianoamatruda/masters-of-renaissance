86e2a45346606e17d8f9c2d08b5d62bbf37807ac
Damiano Amatruda <damiano.amatruda@outlook.com>
2021-04-18 01:42:50 +0200

    Make ActionToken a functional interface

--- src/main/java/it/polimi/ingsw/model/actiontokens/ActionToken.java
+++ src/main/java/it/polimi/ingsw/model/actiontokens/ActionToken.java
@@ -5,6 +5,7 @@ import it.polimi.ingsw.model.SoloGame;
 /**
  * This interface represents an action token, and will be used only in Solo games.
  */
+@FunctionalInterface
 public interface ActionToken {
     /**
      * Triggers the effect of a specific token.
