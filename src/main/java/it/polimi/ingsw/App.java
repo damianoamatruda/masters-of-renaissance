eea8ab1fdea5a706ade3d437fe8d244242916370
alex-atanassov <alessandro.atanassov@mail.polimi.it>
2021-04-23 13:47:56 +0200

    Delete unused config.xml

--- src/main/java/it/polimi/ingsw/App.java
+++ src/main/java/it/polimi/ingsw/App.java
@@ -1,8 +1,12 @@
 package it.polimi.ingsw;
 
 import it.polimi.ingsw.server.controller.FileGameFactory;
-import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
+import it.polimi.ingsw.server.model.Game;
+import it.polimi.ingsw.server.model.GameFactory;
+import it.polimi.ingsw.server.model.Player;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * The main class of the project.
@@ -13,8 +17,11 @@ public class App {
      *
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws IOException, ClassNotFoundException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
-        FileGameFactory f = new FileGameFactory(App.class.getResourceAsStream("/config.json"));
-        System.out.println("");
+    public static void main(String[] args) {
+        GameFactory gameFactory = new FileGameFactory(App.class.getResourceAsStream("/config.json"));
+        Game game = gameFactory.getMultiGame(List.of("PlayerA", "PlayerB", "PlayerC"));
+
+        System.out.println("Players: " + game.getPlayers().stream()
+                .map(Player::getNickname).collect(Collectors.joining(", ")));
     }
 }
