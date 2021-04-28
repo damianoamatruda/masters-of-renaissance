68d956ad59008cd7369674dddbe7b45ebd37bec2
Damiano Amatruda <damiano.amatruda@outlook.com>
2021-04-28 20:35:49 +0200

    Make Lobby independent of communication details

--- src/main/java/it/polimi/ingsw/server/controller/Controller.java
+++ src/main/java/it/polimi/ingsw/server/controller/Controller.java
@@ -2,22 +2,36 @@ package it.polimi.ingsw.server.controller;
 
 import it.polimi.ingsw.server.controller.messages.Message;
 import it.polimi.ingsw.server.model.Lobby;
+import it.polimi.ingsw.server.model.Player;
 
 import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Controller {
     private final Lobby model;
+    final private Map<Socket, Player> players;
 
     public Controller(Lobby model) {
         this.model = model;
+        this.players = new HashMap<>();
+    }
+
+    public void assignPlayer(Socket client, Player player) {
+        players.put(client, player);
+    }
+
+    public Player getPlayer(Socket client) {
+        return players.get(client);
     }
 
     public void joinLobby(Socket client) {
-        model.joinLobby(client);
+        model.joinLobby(getPlayer(client));
     }
 
-    public void handle(Message m, Socket c){
-        m.handle(model.getJoinedGame(c), model.getPlayer(c));
+    public void handle(Message message, Socket client) {
+        Player player = getPlayer(client);
+        message.handle(model.getJoinedGame(player).orElseThrow(), player);
     }
 
     // public void handle(MessageFirstType m, Socket c) {
--- src/main/java/it/polimi/ingsw/server/model/Lobby.java
+++ src/main/java/it/polimi/ingsw/server/model/Lobby.java
@@ -2,40 +2,24 @@ package it.polimi.ingsw.server.model;
 
 import it.polimi.ingsw.server.model.gamecontext.GameContext;
 
-import java.net.Socket;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Lobby {
     final private GameFactory gameFactory;
     final private List<GameContext> games;
-    final private Map<Socket, GameContext> joined;
-    final private Map<Socket, Player> players;
+    final private Map<Player, GameContext> joined;
 
     public Lobby(GameFactory gameFactory) {
         this.gameFactory = gameFactory;
         this.games = new ArrayList<>();
         this.joined = new HashMap<>();
-        this.players = new HashMap<>();
     }
 
-    public void joinLobby(Socket client) {
-        joined.put(client, new GameContext(gameFactory.getSoloGame("")));
+    public void joinLobby(Player player) {
+        joined.put(player, new GameContext(gameFactory.getSoloGame("")));
     }
 
-    public GameContext getJoinedGame(Socket c) {
-        return joined.get(c);
+    public Optional<GameContext> getJoinedGame(Player player) {
+        return Optional.ofNullable(joined.get(player));
     }
-
-    public void assignPlayer(Socket c, Player p) {
-        players.put(c, p);
-    }
-
-    public Player getPlayer(Socket c) {
-        return players.get(c);
-    }
-
-
 }
