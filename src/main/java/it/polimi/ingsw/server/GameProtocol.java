bb9f7a3494d2cdf6025ff1a2838acd21771687f8
Damiano Amatruda <damiano.amatruda@outlook.com>
2021-04-29 18:38:53 +0200

    controller: Handle invalid JSON (bug fix)

--- src/main/java/it/polimi/ingsw/server/GameProtocol.java
+++ src/main/java/it/polimi/ingsw/server/GameProtocol.java
@@ -2,6 +2,7 @@ package it.polimi.ingsw.server;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
 import it.polimi.ingsw.server.controller.Controller;
 import it.polimi.ingsw.server.controller.messages.Message;
 
@@ -15,13 +16,11 @@ public class GameProtocol {
     }
 
     public String processInput(String input, Socket client) {
-        controller.joinLobby(client);
-
-        input = """
+        /*input = """
                 {
                   "type": "ReqDiscardLeader",
                   "leaderId": 1
-                }""";
+                }""";*/
 
         System.out.println("Received: \"" + input + "\"");
         if (input.equals("quit")) return "Bye.";
@@ -29,7 +28,15 @@ public class GameProtocol {
         /* Interprets command string and calls an action from the model */
 
         Gson gson = new Gson();
-        JsonObject jsonObject = gson.fromJson(input, JsonObject.class);
+        JsonObject jsonObject;
+
+        try {
+            jsonObject = gson.fromJson(input, JsonObject.class);
+        } catch (JsonParseException e) {
+            System.err.println("Invalid input.");
+            return input + "?";
+        }
+
         try {
             Message message = gson.fromJson(jsonObject, Class.forName("it.polimi.ingsw.server.controller.messages." + jsonObject.get("type").getAsString()).asSubclass(Message.class));
             message.handle(controller, client);
