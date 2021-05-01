4383b2d618989ed62b9518c9dfa6843a87626737
Damiano Amatruda <damiano.amatruda@outlook.com>
2021-05-01 16:09:37 +0200

    Client: Receive and send asynchronously
    * Uses two threads: one for receiving and one for sending
    * Allows to receive more than one line at a time

--- src/main/java/it/polimi/ingsw/client/Client.java
+++ src/main/java/it/polimi/ingsw/client/Client.java
@@ -17,10 +17,20 @@ public class Client {
 
     private final String host;
     private final int port;
+    private final Socket gameSocket;
 
     public Client(String host, int port) {
         this.host = host;
         this.port = port;
+        Socket gameSocket = null;
+        try {
+            gameSocket = new Socket(host, port);
+        } catch (UnknownHostException e) {
+            hostError();
+        } catch (IOException e) {
+            ioError();
+        }
+        this.gameSocket = gameSocket;
     }
 
     public static void main(String[] args) {
@@ -30,42 +40,57 @@ public class Client {
         String hostName = args.length >= 1 ? args[0] : (jsonConfig = parseConfig.get()).get("host").getAsString();
         int portNumber = args.length >= 2 ? Integer.parseInt(args[1]) : (jsonConfig != null ? jsonConfig : parseConfig.get()).get("port").getAsInt();
 
-        new Client(hostName, portNumber).execute();
+        new Client(hostName, portNumber).start();
     }
 
-    public void execute() {
+    public void start() {
+        new Thread(this::receive).start();
+        new Thread(this::send).start();
+    }
+
+    private void receive() {
         try (
-                Socket gameSocket = new Socket(host, port);
-                PrintWriter out =
-                        new PrintWriter(gameSocket.getOutputStream(), true);
                 BufferedReader in =
                         new BufferedReader(
-                                new InputStreamReader(gameSocket.getInputStream()));
+                                new InputStreamReader(gameSocket.getInputStream()))
+        ) {
+            String fromServer;
+            while ((fromServer = in.readLine()) != null) {
+                System.out.println("Server: " + fromServer);
+                if (fromServer.equals("Bye."))
+                    break;
+            }
+        } catch (IOException e) {
+            ioError();
+        }
+    }
+
+    private void send() {
+        try (
+                PrintWriter out =
+                        new PrintWriter(gameSocket.getOutputStream(), true);
                 BufferedReader stdIn =
                         new BufferedReader(
                                 new InputStreamReader(System.in))
         ) {
-            String fromServer;
-
-            while((fromServer = in.readLine()) != null){
-                System.out.println("Server: " + fromServer);
-                if(fromServer.equals("Bye."))
-                    break;
-
-                String fromUser = stdIn.readLine();
-                if(fromUser != null){
-                    System.out.println("Client: " + fromUser);
-                    out.println(fromUser);
-                }
+            String fromUser;
+            while ((fromUser = stdIn.readLine()) != null) {
+                System.out.println("Client: " + fromUser);
+                out.println(fromUser);
             }
-
-        } catch (UnknownHostException e) {
-            System.err.println("Don't know about host " + host);
-            System.exit(1);
         } catch (IOException e) {
-            System.err.println("Couldn't get I/O for the connection to " +
-                    host);
-            System.exit(1);
+            ioError();
         }
+    }
+
+    private void hostError() {
+        System.err.println("Don't know about host " + host);
+        System.exit(1);
+    }
+
+    private void ioError() {
+        System.err.println("Couldn't get I/O for the connection to " +
+                host);
+        System.exit(1);
     }
 }
