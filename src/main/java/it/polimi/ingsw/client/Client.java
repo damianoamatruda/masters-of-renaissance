df26a1f0d9e45be7432c9f28ca43bd14eb45b03f
Damiano Amatruda <damiano.amatruda@outlook.com>
2021-05-01 17:29:14 +0200

    Refine client

--- src/main/java/it/polimi/ingsw/client/Client.java
+++ src/main/java/it/polimi/ingsw/client/Client.java
@@ -17,46 +17,50 @@ public class Client {
 
     private final String host;
     private final int port;
-    private final Socket gameSocket;
+    private Socket socket;
 
     public Client(String host, int port) {
         this.host = host;
         this.port = port;
-        Socket gameSocket = null;
-        try {
-            gameSocket = new Socket(host, port);
-        } catch (UnknownHostException e) {
-            hostError();
-        } catch (IOException e) {
-            ioError();
-        }
-        this.gameSocket = gameSocket;
     }
 
     public static void main(String[] args) {
         JsonObject jsonConfig = null;
         Supplier<JsonObject> parseConfig = () -> new Gson().fromJson(new InputStreamReader(Objects.requireNonNull(Client.class.getResourceAsStream(jsonConfigPath))), JsonObject.class);
 
-        String hostName = args.length >= 1 ? args[0] : (jsonConfig = parseConfig.get()).get("host").getAsString();
-        int portNumber = args.length >= 2 ? Integer.parseInt(args[1]) : (jsonConfig != null ? jsonConfig : parseConfig.get()).get("port").getAsInt();
+        String host = args.length >= 1 ? args[0] : (jsonConfig = parseConfig.get()).get("host").getAsString();
+        int port = args.length >= 2 ? Integer.parseInt(args[1]) : (jsonConfig != null ? jsonConfig : parseConfig.get()).get("port").getAsInt();
 
-        new Client(hostName, portNumber).start();
+        try {
+            new Client(host, port).start();
+        } catch (IOException e) {
+            System.exit(1);
+        }
     }
 
-    public void start() {
+    public void start() throws IOException {
+        try {
+            socket = new Socket(host, port);
+        } catch (UnknownHostException e) {
+            hostError();
+            throw e;
+        } catch (IOException e) {
+            ioError();
+            throw e;
+        }
+
+        // TODO: End all threads when one ends
         new Thread(this::receive).start();
         new Thread(this::send).start();
+
+        // socket.close();
     }
 
     private void receive() {
-        try (
-                BufferedReader in =
-                        new BufferedReader(
-                                new InputStreamReader(gameSocket.getInputStream()))
-        ) {
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
             String fromServer;
             while ((fromServer = in.readLine()) != null) {
-                System.out.println("Server: " + fromServer);
+                System.out.println(fromServer);
                 if (fromServer.equals("Bye."))
                     break;
             }
@@ -66,18 +70,11 @@ public class Client {
     }
 
     private void send() {
-        try (
-                PrintWriter out =
-                        new PrintWriter(gameSocket.getOutputStream(), true);
-                BufferedReader stdIn =
-                        new BufferedReader(
-                                new InputStreamReader(System.in))
-        ) {
-            String fromUser;
-            while ((fromUser = stdIn.readLine()) != null) {
-                System.out.println("Client: " + fromUser);
-                out.println(fromUser);
-            }
+        try (PrintWriter out = new PrintWriter(socket.getOutputStream(), true)) {
+            BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+            String fromClient;
+            while ((fromClient = stdIn.readLine()) != null)
+                out.println(fromClient);
         } catch (IOException e) {
             ioError();
         }
@@ -85,12 +82,9 @@ public class Client {
 
     private void hostError() {
         System.err.println("Don't know about host " + host);
-        System.exit(1);
     }
 
     private void ioError() {
-        System.err.println("Couldn't get I/O for the connection to " +
-                host);
-        System.exit(1);
+        System.err.println("Couldn't get I/O for the connection to " + host);
     }
 }
