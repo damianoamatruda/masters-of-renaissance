544b4d9a987c4978d2372b87d9dec5fec4b61a8b
Damiano Amatruda <damiano.amatruda@outlook.com>
2021-05-11 15:02:08 +0200

    Add Javadoc in package-info.java of mvevents and vcevents

--- src/main/java/it/polimi/ingsw/common/events/mvevents/package-info.java
+++ src/main/java/it/polimi/ingsw/common/events/mvevents/package-info.java
@@ -1,1 +1,4 @@
+/**
+ * Classes that implement the events from the model to a view of the Masters of Renaissance game.
+ */
 package it.polimi.ingsw.common.events.mvevents;
--- src/main/java/it/polimi/ingsw/common/events/vcevents/package-info.java
+++ src/main/java/it/polimi/ingsw/common/events/vcevents/package-info.java
@@ -1,1 +1,4 @@
+/**
+ * Classes that implement the events from a view to the controller of the Masters of Renaissance game.
+ */
 package it.polimi.ingsw.common.events.vcevents;
