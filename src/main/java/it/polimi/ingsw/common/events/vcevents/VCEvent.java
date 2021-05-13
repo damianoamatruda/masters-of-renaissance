aff28ae9c5663c60bff62c932fabe918cf21b40c
alex-atanassov <alessandro.atanassov@mail.polimi.it>
2021-05-13 11:03:40 +0200

    Remove redeclaration of method handle in Event subinterfaces

--- src/main/java/it/polimi/ingsw/common/events/vcevents/VCEvent.java
+++ src/main/java/it/polimi/ingsw/common/events/vcevents/VCEvent.java
@@ -1,8 +1,7 @@
 package it.polimi.ingsw.common.events.vcevents;
 
-import it.polimi.ingsw.common.View;
 import it.polimi.ingsw.common.events.Event;
 
 public interface VCEvent extends Event {
-    void handle(View view);
+
 }
