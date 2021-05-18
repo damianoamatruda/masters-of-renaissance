e9454058a525634cc87a97cfc758a5769118b443
alex-atanassov <alessandro.atanassov@mail.polimi.it>
2021-05-18 19:44:59 +0200

    Add package for networking events

---
+++ src/main/java/it/polimi/ingsw/common/events/netevents/NetEvent.java
@@ -0,0 +1,6 @@
+package it.polimi.ingsw.common.events.netevents;
+
+import it.polimi.ingsw.common.events.Event;
+
+public interface NetEvent extends Event {
+}
---
+++ src/main/java/it/polimi/ingsw/common/events/netevents/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Classes that implement the events concerning networking.
+ */
+package it.polimi.ingsw.common.events.netevents;
