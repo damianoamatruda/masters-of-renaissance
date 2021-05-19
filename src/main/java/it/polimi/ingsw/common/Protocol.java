5e7421cc06c48fc2675cb83d2c36b946b7373918
Marco Ciarrocca <marco.ciarrocca@mekomi.dev>
2021-05-19 10:08:12 +0200

    Refactor sender GSON instance to not be recreated at every message

--- src/main/java/it/polimi/ingsw/common/Protocol.java
+++ src/main/java/it/polimi/ingsw/common/Protocol.java
@@ -6,6 +6,17 @@ import it.polimi.ingsw.common.events.mvevents.MVEvent;
 import it.polimi.ingsw.common.events.vcevents.VCEvent;
 
 public class Protocol {
+    private Gson senderGson = new Gson().newBuilder()
+                    .enableComplexMapKeySerialization()
+                    .registerTypeHierarchyAdapter(Event.class, (JsonSerializer<Event>) (msg, type, jsonSerializationContext) -> {
+                        Gson gson1 = new Gson().newBuilder()
+                                .enableComplexMapKeySerialization()
+                                .create();
+                        JsonElement jsonElement = gson1.toJsonTree(msg);
+                        jsonElement.getAsJsonObject().addProperty("type", ((Class<?>) type).getSimpleName());
+                        return jsonElement;
+                    })
+                    .create();
     /*
      * Interprets input string.
      */
@@ -51,17 +62,6 @@ public class Protocol {
     }
 
     public String processOutput(Event event) {
-        Gson gson = new Gson().newBuilder()
-                .enableComplexMapKeySerialization()
-                .registerTypeHierarchyAdapter(Event.class, (JsonSerializer<Event>) (msg, type, jsonSerializationContext) -> {
-                    Gson gson1 = new Gson().newBuilder()
-                            .enableComplexMapKeySerialization()
-                            .create();
-                    JsonElement jsonElement = gson1.toJsonTree(msg);
-                    jsonElement.getAsJsonObject().addProperty("type", ((Class<?>) type).getSimpleName());
-                    return jsonElement;
-                })
-                .create();
-        return gson.toJson(event);
+        return senderGson.toJson(event);
     }
 }
