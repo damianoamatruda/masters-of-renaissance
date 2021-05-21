f0fcdc59ea41ece0d81f3ae5c303b1407aef78ad
Marco Ciarrocca <marco.ciarrocca@mekomi.dev>
2021-05-21 14:08:27 +0200

    Small fix

--- src/main/java/it/polimi/ingsw/client/cli/CliView.java
+++ src/main/java/it/polimi/ingsw/client/cli/CliView.java
@@ -236,7 +236,7 @@ public class CliView extends View implements EventListener<VCEvent> {
     }
 
     private void on(UpdateBookedSeats event) {
-        if(event.canPrepareNewGame().equals(cache.getNickname()) && !(cli.getState() instanceof InputPlayersCountState))
+        if(event.canPrepareNewGame().equals(cache.getNickname()) && (!(cli.getState() instanceof InputPlayersCountState) || !(cli.getState() instanceof WaitingBeforeGameState)))
             cli.setState(new InputPlayersCountState());
         else cli.setState(new WaitingBeforeGameState(event.getBookedSeats()));
     }
