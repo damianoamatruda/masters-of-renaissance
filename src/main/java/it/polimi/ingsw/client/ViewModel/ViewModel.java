3774079bad3d88c153d055aa9267f7076522d625
Marco Ciarrocca <marco.ciarrocca@mekomi.dev>
2021-05-22 16:54:33 +0200

    Refactor VM fields to final

--- src/main/java/it/polimi/ingsw/client/ViewModel/ViewModel.java
+++ src/main/java/it/polimi/ingsw/client/ViewModel/ViewModel.java
@@ -1,5 +1,6 @@
 package it.polimi.ingsw.client.ViewModel;
 
+import java.util.HashMap;
 import java.util.Map;
 
 /** Data storage cache on the Masters Of Renaissance client. */
@@ -9,60 +10,46 @@ public class ViewModel {
      * 
      * E.g. state-aiding variables, old requests...
      */
-    private UIData uiData;
+    private final UIData uiData;
     /**
      * Game-global objects sent by the server.
      * Object specifications are here. For ownership, check PlayerData
      * 
      * E.g. cards, resources, containers...
      */
-    private GameData gameData;
+    private final GameData gameData;
     /** 
      * Player data.
      * 
      * E.g. ownership of objects, victory points...
      */
-    private Map<String, PlayerData> playerData;
+    private final Map<String, PlayerData> playerData;
+
+    public ViewModel() {
+        this.uiData = new UIData();
+        this.gameData = new GameData();
+        this.playerData = new HashMap<>();
+    }
     
     /**
-     * @return the uiData
+     * @return the ui data
      */
     public UIData getUiData() {
         return uiData;
     }
     
     /**
-     * @param uiData the uiData to set
-     */
-    public void setUiData(UIData uiData) {
-        this.uiData = uiData;
-    }
-
-    /**
-     * @return the gameData
+     * @return the game data
      */
     public GameData getGameData() {
         return gameData;
     }
     
     /**
-     * @param gameData the gameData to set
+     * @param nickname the nickname of the player whose data needs to be retrieved.
+     * @return the playerData of the specified player
      */
-    public void setGameData(GameData gameData) {
-        this.gameData = gameData;
-    }
-    
-    /**
-     * @return the playerData
-     */
-    public Map<String, PlayerData> getPlayerData() {
-        return playerData;
-    }
-    
-    /**
-     * @param playerData the playerData to set
-     */
-    public void setPlayerData(Map<String, PlayerData> playerData) {
-        this.playerData = playerData;
+    public PlayerData getPlayerData(String nickname) {
+        return playerData.get(nickname);
     }
 }
