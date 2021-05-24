02c3c200df51ed07441fbc5077459128ec41808e
Damiano Amatruda <damiano.amatruda@outlook.com>
2021-05-24 12:56:31 +0200

    GUI: Add back button in multiplayer

--- src/main/java/it/polimi/ingsw/client/gui/MultiplayerController.java
+++ src/main/java/it/polimi/ingsw/client/gui/MultiplayerController.java
@@ -2,6 +2,7 @@ package it.polimi.ingsw.client.gui;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 
 import java.io.IOException;
 
@@ -11,5 +12,9 @@ public class MultiplayerController {
 
     public void handleServerChoice() throws IOException {
         System.out.print("Input = " + server.getText());
+    }
+
+    public void handleBack(MouseEvent mouseEvent) throws IOException {
+        Gui.setRoot("mainmenu");
     }
 }
