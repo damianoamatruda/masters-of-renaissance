a0d7257a7b278f0164de4b36953834640a73789d
Marco Ciarrocca <marco.ciarrocca@mekomi.dev>
2021-05-26 18:59:30 +0200

    Make leader cards show productions

--- src/main/java/it/polimi/ingsw/client/gui/SetupLeadersController.java
+++ src/main/java/it/polimi/ingsw/client/gui/SetupLeadersController.java
@@ -1,10 +1,14 @@
 package it.polimi.ingsw.client.gui;
 
 import it.polimi.ingsw.client.gui.components.LeaderCard;
+import it.polimi.ingsw.common.reducedmodel.ReducedResourceTransactionRecipe;
+import it.polimi.ingsw.common.reducedmodel.ReducedResourceType;
 import javafx.scene.layout.HBox;
 
 import java.net.URL;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.ResourceBundle;
 
 public class SetupLeadersController extends GuiController {
@@ -18,11 +22,18 @@ public class SetupLeadersController extends GuiController {
 
         if (gui.getViewModel().getPlayerLeaderCards(gui.getViewModel().getUiData().getLocalPlayerNickname()) == null)
             throw new RuntimeException();
+        
+        Map<String, Integer> m1 = new HashMap<>();
+        Map<String, Integer> m2 = Map.of("shield", 2);
+        m1.put("coin", 1);
+        m1.put("Shield", 2);
+        ReducedResourceTransactionRecipe p1 = new ReducedResourceTransactionRecipe(0, m1, 1, null, m2, 2, null, false);
 
         List<LeaderCard> leaderCards = gui.getViewModel().getPlayerLeaderCards(gui.getViewModel().getUiData().getLocalPlayerNickname()).stream().map(reducedLeader -> {
             LeaderCard leaderCard = new LeaderCard();
             leaderCard.setLeaderType(reducedLeader.getLeaderType());
             leaderCard.setResourceType(reducedLeader.getResourceType());
+            leaderCard.setProduction(p1);
             return leaderCard;
         }).toList();
 
--- src/main/java/it/polimi/ingsw/client/gui/components/LeaderCard.java
+++ src/main/java/it/polimi/ingsw/client/gui/components/LeaderCard.java
@@ -2,16 +2,27 @@ package it.polimi.ingsw.client.gui.components;
 
 import javafx.beans.property.StringProperty;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderStroke;
+import javafx.scene.layout.BorderStrokeStyle;
+import javafx.scene.layout.BorderWidths;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 
 import java.io.IOException;
+
+import it.polimi.ingsw.common.reducedmodel.ReducedResourceTransactionRecipe;
 
 public class LeaderCard extends VBox {
     public Pane leaderCard;
     public Text leaderTypeText;
     public Text resourceTypeText;
+    public Pane resourcePane;
+
+    private Production prod;
 
     public LeaderCard() {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/assets/gui/components/leadercard.fxml"));
@@ -23,6 +34,18 @@ public class LeaderCard extends VBox {
         } catch (IOException exception) {
             throw new RuntimeException(exception);
         }
+    }
+
+    public void setProduction(ReducedResourceTransactionRecipe prod) {
+        this.prod = new Production();
+        this.prod.setProduction(prod);
+
+        this.prod.maxWidthProperty().bind(resourcePane.maxWidthProperty());
+        this.prod.maxHeightProperty().bind(resourcePane.maxHeightProperty());
+
+        resourcePane.getChildren().add(this.prod);
+        resourcePane.setBorder(new Border(new BorderStroke(Color.BLACK, 
+            BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT)));
     }
 
     public String getLeaderType() {
