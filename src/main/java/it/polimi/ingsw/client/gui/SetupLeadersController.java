c077c4a4603d4b6a74f15d5f863b8eeb6c03f012
alex-atanassov <alessandro.atanassov@mail.polimi.it>
2021-05-27 13:45:53 +0200

    Create GUI component warehouse

--- src/main/java/it/polimi/ingsw/client/gui/SetupLeadersController.java
+++ src/main/java/it/polimi/ingsw/client/gui/SetupLeadersController.java
@@ -1,16 +1,15 @@
 package it.polimi.ingsw.client.gui;
 
 import it.polimi.ingsw.client.gui.components.LeaderCard;
+import it.polimi.ingsw.client.gui.components.Warehouse;
+import it.polimi.ingsw.common.reducedmodel.ReducedResourceContainer;
 import it.polimi.ingsw.common.reducedmodel.ReducedResourceTransactionRecipe;
 import it.polimi.ingsw.common.reducedmodel.ReducedResourceType;
 import javafx.geometry.Pos;
 import javafx.scene.layout.HBox;
 
 import java.net.URL;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.ResourceBundle;
+import java.util.*;
 
 public class SetupLeadersController extends GuiController {
     public HBox leadersContainer;
@@ -30,22 +29,31 @@ public class SetupLeadersController extends GuiController {
         m1.put("Shield", 2);
         ReducedResourceTransactionRecipe p1 = new ReducedResourceTransactionRecipe(0, m1, 1, null, m2, 2, null, false);
 
-        List<LeaderCard> leaderCards = gui.getViewModel().getPlayerLeaderCards(gui.getViewModel().getUiData().getLocalPlayerNickname()).stream().map(reducedLeader -> {
-            LeaderCard leaderCard = new LeaderCard(reducedLeader.getLeaderType());
-            leaderCard.setLeaderType(reducedLeader.getLeaderType());
-            leaderCard.setVictoryPoints(reducedLeader.getVictoryPoints()+"");
-            leaderCard.setResourceType(reducedLeader.getResourceType());
-            leaderCard.setRequirement(reducedLeader.getResourceRequirement());
-            leaderCard.setRequirement(reducedLeader.getDevCardRequirement());
-            leaderCard.setProduction(p1);
-            return leaderCard;
-        }).toList();
-
-        System.out.println(leaderCards);
+//        List<LeaderCard> leaderCards = gui.getViewModel().getPlayerLeaderCards(gui.getViewModel().getUiData().getLocalPlayerNickname()).stream().map(reducedLeader -> {
+//            LeaderCard leaderCard = new LeaderCard(reducedLeader.getLeaderType());
+//            leaderCard.setLeaderType(reducedLeader.getLeaderType());
+//            leaderCard.setVictoryPoints(reducedLeader.getVictoryPoints()+"");
+//            leaderCard.setResourceType(reducedLeader.getResourceType());
+//            leaderCard.setRequirement(reducedLeader.getResourceRequirement());
+//            leaderCard.setRequirement(reducedLeader.getDevCardRequirement());
+//            leaderCard.setProduction(p1);
+//            return leaderCard;
+//        }).toList();
+//
+//        System.out.println(leaderCards);
 
         leadersContainer.setSpacing(10);
         leadersContainer.setAlignment(Pos.CENTER);
 
-        leadersContainer.getChildren().addAll(leaderCards);
+//        leadersContainer.getChildren().addAll(leaderCards);
+
+        Warehouse w = new Warehouse();
+        List<ReducedResourceContainer> containers = new ArrayList<>();
+        containers.add(new ReducedResourceContainer(0, 1, Map.of("Coin", 1), "Coin"));
+        containers.add(new ReducedResourceContainer(0, 2, Map.of("Shield", 2), "Shield"));
+        containers.add(new ReducedResourceContainer(0, 3, Map.of(), null));
+
+        w.setWarehouseShelves(containers);
+        leadersContainer.getChildren().add(w);
     }
 }
---
+++ src/main/java/it/polimi/ingsw/client/gui/components/Strongbox.java
@@ -0,0 +1,4 @@
+package it.polimi.ingsw.client.gui.components;
+
+public class Strongbox {
+}
