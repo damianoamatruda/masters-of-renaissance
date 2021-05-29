47cb285fce8efe40cb1469a8ee66f9787d984be7
Marco Ciarrocca <marco.ciarrocca@mekomi.dev>
2021-05-29 19:37:44 +0200

    Add UpdateCurrentPlayer listener to WaitingAfterTurnState

--- src/main/java/it/polimi/ingsw/client/cli/WaitingAfterTurnState.java
+++ src/main/java/it/polimi/ingsw/client/cli/WaitingAfterTurnState.java
@@ -1,5 +1,7 @@
 package it.polimi.ingsw.client.cli;
 
+import it.polimi.ingsw.common.events.mvevents.UpdateCurrentPlayer;
+
 public class WaitingAfterTurnState extends CliState {
     @Override
     public void render(Cli cli) {
@@ -7,5 +9,12 @@ public class WaitingAfterTurnState extends CliState {
             cli.getOut().println("Please wait for other players to end their turn...");
         else
             cli.getOut().println("You have played your last turn. Waiting for others to finish...");
+    }
+
+    @Override
+    public void on(Cli cli, UpdateCurrentPlayer event) {
+        super.on(cli, event);
+        if (event.getPlayer().equals(cli.getViewModel().getLocalPlayerNickname()))
+            cli.setState(new TurnBeforeActionState());
     }
 }
