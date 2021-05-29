00e367f6714b798c320637ecb3fce4394470a6e1
Marco Ciarrocca <marco.ciarrocca@mekomi.dev>
2021-05-29 19:38:27 +0200

    Refactor swapShelves to be allowed at any time during a turn

--- src/main/java/it/polimi/ingsw/client/cli/TurnBeforeActionState.java
+++ src/main/java/it/polimi/ingsw/client/cli/TurnBeforeActionState.java
@@ -2,7 +2,6 @@ package it.polimi.ingsw.client.cli;
 
 import it.polimi.ingsw.client.cli.components.Menu;
 import it.polimi.ingsw.common.events.mvevents.*;
-import it.polimi.ingsw.common.events.vcevents.ReqSwapShelves;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -33,47 +32,13 @@ public class TurnBeforeActionState extends CliTurnState {
         new Menu(entries).render(cli);
     }
 
-    private void swapShelves(Cli cli) {
-        cli.getOut().println("Swapping shelves:");
-
-        cli.getPrinter().showWarehouseShelves(cli.getViewModel().getLocalPlayerNickname());
-
-        int shelfid1, shelfid2;
-        boolean isValid = false;
-
-
-        while (!isValid) {
-            String input = cli.prompt("Choose shelf 1");
-            try {
-                shelfid1 = Integer.parseInt(input);
-            } catch (NumberFormatException e) {
-                cli.getOut().println("Please input an integer.");
-                continue;
-            }
-
-            input = cli.prompt("Choose shelf 2");
-            try {
-                shelfid2 = Integer.parseInt(input);
-            } catch (NumberFormatException e) {
-                cli.getOut().println("Please input an integer.");
-                continue;
-            }
-
-            isValid = true;
-            cli.dispatch(new ReqSwapShelves(shelfid1, shelfid2));
-
-            cli.repeatState("Swap request sent...");
-        }
-    }
-
     @Override
     public void on(Cli cli, UpdateSetupDone event) {
-        cli.getViewModel().setSetupDone(true);
         cli.setState(new TurnBeforeActionState());
     }
 
     @Override
     public void on(Cli cli, UpdateCurrentPlayer event) {
-        
+        System.out.println("updcurrpl turnbeforeaction");
     }
 }
