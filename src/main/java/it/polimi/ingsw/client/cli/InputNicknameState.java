ca29cdba80a85d39541e216cc40d2b9eef1d463e
Marco Ciarrocca <marco.ciarrocca@mekomi.dev>
2021-05-29 13:30:42 +0200

    Fix InputNicknameState

--- src/main/java/it/polimi/ingsw/client/cli/InputNicknameState.java
+++ src/main/java/it/polimi/ingsw/client/cli/InputNicknameState.java
@@ -1,8 +1,10 @@
 package it.polimi.ingsw.client.cli;
 
 import it.polimi.ingsw.common.events.mvevents.UpdateBookedSeats;
+import it.polimi.ingsw.common.events.mvevents.UpdateCurrentPlayer;
 import it.polimi.ingsw.common.events.mvevents.UpdateGame;
 import it.polimi.ingsw.common.events.mvevents.UpdateJoinGame;
+import it.polimi.ingsw.common.events.mvevents.UpdateLeadersHand;
 import it.polimi.ingsw.common.events.mvevents.errors.ErrNickname;
 import it.polimi.ingsw.common.events.vcevents.ReqJoin;
 import it.polimi.ingsw.common.events.vcevents.ReqNewGame;
@@ -34,45 +36,15 @@ public class InputNicknameState extends CliState{
 
     @Override
     public void on(Cli cli, UpdateBookedSeats event) {
-        if (cli.isOffline())
-            cli.dispatch(new ReqNewGame(1));
-        else {
-            if (event.canPrepareNewGame() == cli.getViewModel().getLocalPlayerNickname())
-                cli.setState(new InputPlayersCountState());
-            else
-                cli.getOut().printf("%d players waiting for a new game...", event.getBookedSeats());
-        }
+        if (event.canPrepareNewGame().equals(cli.getViewModel().getLocalPlayerNickname()))
+            cli.setState(new InputPlayersCountState());
+        else
+            cli.getOut().printf("%d players waiting for a new game...", event.getBookedSeats());
     }
 
     @Override
     public void on(Cli cli, UpdateJoinGame event) {
-        cli.getOut().printf("A new player joined the game! Getting to %d...", event.getPlayersCount());
+        if (!cli.isOffline())
+            cli.getOut().printf("A new player joined the game! Getting to %d...", event.getPlayersCount());
     }
-
-    @Override
-    public void on(Cli cli, UpdateGame event) {
-        super.on(cli, event); // need to update the cache
-        if (event.isResumed())
-            cli.setState(new WaitingAfterTurnState());
-        else
-            cli.setState(
-                new SetupLeadersState(cli.getViewModel()
-                    .getLocalPlayerData()
-                    .getSetup()
-                    .getChosenLeadersCount()));
-    }
-
-    // decided to implement it at the CliState level, if issues bring it down a level
-    // @Override
-    // public void on(Cli cli, UpdateCurrentPlayer event) {
-    //     // having this overriding may prove necessary:
-    //     // if the setState isn't fast enough, the next event after UpdateGame is CurrentPlayer and
-    //     // this means it might not get handled in WaitingAfterTurnState
-    //     super.on(cli, event);
-
-    //     if (event.getPlayer().equals(cli.getCache().getUiData().getLocalPlayerNickname()))
-    //         cli.setState(new TurnBeforeActionState());
-    //     else
-    //         cli.setState(new WaitingAfterTurnState());
-    // }
 }
