4ff079a6c821330980ce9db60d895b694b1cb4a9
Damiano Amatruda <damiano.amatruda@outlook.com>
2021-05-30 16:38:18 +0200

    CLI: Use '1' and '2' instead of 'S' and 'M' in main menu

--- src/main/java/it/polimi/ingsw/client/cli/MainMenuState.java
+++ src/main/java/it/polimi/ingsw/client/cli/MainMenuState.java
@@ -17,8 +17,8 @@ public class MainMenuState extends CliState {
 
     private void renderMainMenu(Cli cli) {
         Map<Character, Menu.Entry> entries = new LinkedHashMap<>();
-        entries.put('S', new Menu.Entry("Play Offline", cli1 -> cli.setState(new PlayOfflineState())));
-        entries.put('M', new Menu.Entry("Play Online", cli1 -> cli.setState(new PlayOnlineState())));
+        entries.put('1', new Menu.Entry("Play Offline", cli1 -> cli.setState(new PlayOfflineState())));
+        entries.put('2', new Menu.Entry("Play Online", cli1 -> cli.setState(new PlayOnlineState())));
         entries.put('O', new Menu.Entry("Options...", cli1 -> cli.getOut().println("Launching Options...")));
         entries.put('Q', new Menu.Entry("Quit Game", Cli::quit));
         new Menu(entries).render(cli);
