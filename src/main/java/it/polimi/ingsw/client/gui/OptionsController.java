d4400a5e6395ad1bf47ae83b911781027ec2fd2e
Marco Ciarrocca <marco.ciarrocca@mekomi.dev>
2021-06-03 14:01:27 +0200

    Add Options scene

--- src/main/java/it/polimi/ingsw/client/gui/OptionsController.java
+++ src/main/java/it/polimi/ingsw/client/gui/OptionsController.java
@@ -1,4 +1,12 @@
 package it.polimi.ingsw.client.gui;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Slider;
+
 public class OptionsController extends GuiController {
+    @FXML private Slider musicSlider;
+    @FXML private Slider soundFXSlider;
+    @FXML private Button configBtn;
+
 }
