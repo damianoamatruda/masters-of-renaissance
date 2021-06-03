03286a25ac2797566beead972dbc3b74cd839b37
alex-atanassov <alessandro.atanassov@mail.polimi.it>
2021-06-03 19:11:27 +0200

    Fix base production printing in CLI

--- src/main/java/it/polimi/ingsw/client/cli/components/ResourceTransactionRecipe.java
+++ src/main/java/it/polimi/ingsw/client/cli/components/ResourceTransactionRecipe.java
@@ -35,10 +35,11 @@ public class ResourceTransactionRecipe extends StringComponent {
         if (reducedResourceTransactionRecipe.getInputBlanks() > 0) {
             row += String.format("  %-23s", String.format("Blanks: %d", reducedResourceTransactionRecipe.getInputBlanks()));
             column.add(row);
+            row = "";
         } else if (row.length() > 0) column.add(row + " ".repeat(25));
         if (!reducedResourceTransactionRecipe.getInputBlanksExclusions().isEmpty()) {
             column.add("B. exclusions:");
-            reducedResourceTransactionRecipe.getInputBlanksExclusions().forEach(e -> column.add(new Resource(e).getString(cli)));
+            reducedResourceTransactionRecipe.getInputBlanksExclusions().forEach(e -> column.add("  " + new Resource(e).getString(cli)));
         }
         column.add("Output:");
 
@@ -57,10 +58,11 @@ public class ResourceTransactionRecipe extends StringComponent {
         if (reducedResourceTransactionRecipe.getOutputBlanks() > 0) {
             row += String.format("  %-23s", String.format("Blanks: %d", reducedResourceTransactionRecipe.getOutputBlanks()));
             column.add(row);
+            row = "";
         } else if (row.length() > 0) column.add(row + " ".repeat(25));
         if (!reducedResourceTransactionRecipe.getOutputBlanksExclusions().isEmpty()) {
             column.add("B. exclusions:");
-            reducedResourceTransactionRecipe.getOutputBlanksExclusions().forEach(e -> column.add(new Resource(e).getString(cli)));
+            reducedResourceTransactionRecipe.getOutputBlanksExclusions().forEach(e -> column.add("  " + new Resource(e).getString(cli)));
         }
         column.add(reducedResourceTransactionRecipe.isDiscardableOutput() ? "Output is discardable" : " ");
 
