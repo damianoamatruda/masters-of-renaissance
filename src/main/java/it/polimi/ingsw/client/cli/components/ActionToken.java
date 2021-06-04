39c8787caabcef808ed41784c435086ef37b0759
Damiano Amatruda <damiano.amatruda@outlook.com>
2021-06-04 23:00:22 +0200

    CLI: Fix heading of component 'ActionToken'

--- src/main/java/it/polimi/ingsw/client/cli/components/ActionToken.java
+++ src/main/java/it/polimi/ingsw/client/cli/components/ActionToken.java
@@ -13,7 +13,7 @@ public class ActionToken implements Renderable {
 
     @Override
     public void render(Cli cli) {
-        cli.getOut().printf("--- ActionToken (ID: \u001B[1m\u001B[37m%d\u001B[0m)%n", reducedActionToken.getId());
+        cli.getOut().printf("--- ActionToken (ID: \u001B[1m\u001B[37m%d\u001B[0m) ---%n", reducedActionToken.getId());
         cli.getOut().printf("Kind: %s%n", reducedActionToken.getKind());
         if (reducedActionToken.getDiscardedDevCardColor() != null)
             cli.getOut().printf("Discarded development card of color %s%n", new DevCardColor(reducedActionToken.getDiscardedDevCardColor()).getString(cli));
