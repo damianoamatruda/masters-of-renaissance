c0e48bcfbd86a3779b2dcaa87db5485da3e48e49
Damiano Amatruda <damiano.amatruda@outlook.com>
2021-06-06 14:05:07 +0200

    CLI: Make input of Menu case-insensitive

--- src/main/java/it/polimi/ingsw/client/cli/components/Menu.java
+++ src/main/java/it/polimi/ingsw/client/cli/components/Menu.java
@@ -3,6 +3,7 @@ package it.polimi.ingsw.client.cli.components;
 import it.polimi.ingsw.client.cli.Cli;
 import it.polimi.ingsw.client.cli.Renderable;
 
+import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.function.Consumer;
@@ -11,19 +12,25 @@ public class Menu implements Renderable {
     private final Map<Character, Entry> entries;
 
     public Menu(Map<Character, Entry> entries) {
-        this.entries = new LinkedHashMap<>(entries);
+        if (!entries.keySet().stream().map(Character::toLowerCase).allMatch(new HashSet<>()::add))
+            throw new IllegalArgumentException("Duplicate entries.");
+
+        this.entries = new LinkedHashMap<>();
+        for (Character c : entries.keySet())
+            this.entries.put(Character.toUpperCase(c), entries.get(c));
     }
 
     @Override
     public void render(Cli cli) {
         StringBuilder stringBuilder = new StringBuilder();
-        entries.forEach((character, entry) -> stringBuilder.append(String.format("[%c] %s%n", character, entry.description)));
+        entries.forEach((c, entry) -> stringBuilder.append(String.format("[%c] %s%n", c, entry.description)));
         cli.getOut().print(Cli.center(stringBuilder.toString()));
-        String value;
+        Character c;
         do {
-            value = cli.prompt("");
-        } while (value.isBlank() || !entries.containsKey(value.charAt(0)));
-        entries.get(value.charAt(0)).action.accept(cli);
+            String value = cli.prompt("");
+            c = !value.isBlank() ? Character.toUpperCase(value.charAt(0)) : null;
+        } while (c == null || !entries.containsKey(c));
+        entries.get(c).action.accept(cli);
     }
 
     public static class Entry {
