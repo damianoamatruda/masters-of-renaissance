cfd8273ed5f3e59ac2d25daa7e11020433ba158e
Marco Ciarrocca <marco.ciarrocca@mekomi.dev>
2021-06-08 20:13:36 +0200

    GUI: Add Alert background

--- src/main/java/it/polimi/ingsw/client/gui/components/Alert.java
+++ src/main/java/it/polimi/ingsw/client/gui/components/Alert.java
@@ -1,7 +1,15 @@
 package it.polimi.ingsw.client.gui.components;
 
+import java.util.Objects;
+
 import javafx.geometry.Pos;
 import javafx.scene.Node;
+import javafx.scene.image.Image;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundPosition;
+import javafx.scene.layout.BackgroundRepeat;
+import javafx.scene.layout.BackgroundSize;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
@@ -23,9 +31,23 @@ public class Alert extends BorderPane {
 
         this.setCenter(canvas);
         this.setBottom(new SButton("OK"));
+
+        setBackground();
     }
 
     public Alert(Node center, Node top, Node right, Node left) {
         super(center, top, right, null, left);
+        setBackground();
+    }
+
+    private void setBackground() {
+        Image backBGImage = new Image(
+            Objects.requireNonNull(getClass().getResource("/assets/gui/background.png")).toExternalForm());
+        
+        BackgroundImage backBG = new BackgroundImage(backBGImage,
+            BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER,
+            new BackgroundSize(1.0, 1.0, true, true, false, true));
+        Background bg = new Background(backBG);
+        this.setBackground(bg);
     }
 }
