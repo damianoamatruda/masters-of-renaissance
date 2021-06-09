b365cceeeb5cb6f3e2f82b6b22533a47c354900f
Damiano Amatruda <damiano.amatruda@outlook.com>
2021-06-10 00:40:23 +0200

    Refine OfflineClient
    Related to 2d6e747f48f74fbaa6bc1531a45737c22659f2a7.

--- src/main/java/it/polimi/ingsw/client/OfflineClient.java
+++ src/main/java/it/polimi/ingsw/client/OfflineClient.java
@@ -19,7 +19,6 @@ public class OfflineClient implements Network {
     private final InputStream gameConfigStream;
     private final ExecutorService executor;
     private Controller controller;
-    private View virtualView;
     private NetworkHandler networkHandler;
 
     public OfflineClient(View view, InputStream gameConfigStream) {
@@ -27,7 +26,6 @@ public class OfflineClient implements Network {
         this.gameConfigStream = gameConfigStream != null ? gameConfigStream : getClass().getResourceAsStream(defaultGameConfigPath);
         this.executor = Executors.newCachedThreadPool();
         this.controller = null;
-        this.virtualView = null;
         this.networkHandler = null;
     }
 
@@ -57,24 +55,15 @@ public class OfflineClient implements Network {
         executor.shutdownNow();
 
         if (controller != null) {
-            if (virtualView != null)
-                controller.unregisterOnVC(virtualView);
+            controller.unregisterOnVC(view);
             controller = null;
         }
 
-        if (virtualView != null) {
-            if (networkHandler != null)
-                virtualView.unregisterOnVC(networkHandler);
-
-            view.unregisterOnModelLobby(virtualView);
-            view.unregisterOnModelGameContext(virtualView);
-            view.unregisterOnModelGame(virtualView);
-            view.unregisterOnModelPlayer(virtualView);
-            virtualView = null;
-        }
-
         if (networkHandler != null) {
-            networkHandler.unregisterOnVC(view);
+            view.unregisterOnModelLobby(networkHandler);
+            view.unregisterOnModelGameContext(networkHandler);
+            view.unregisterOnModelGame(networkHandler);
+            view.unregisterOnModelPlayer(networkHandler);
             networkHandler.stop();
             networkHandler = null;
         }
