e054f0e5c298d086faed755222148856c117b6d6
Damiano Amatruda <damiano.amatruda@outlook.com>
2021-06-14 16:53:18 +0200

    Refine methods of AsynchronousEventDispatcher

--- src/main/java/it/polimi/ingsw/common/AsynchronousEventDispatcher.java
+++ src/main/java/it/polimi/ingsw/common/AsynchronousEventDispatcher.java
@@ -4,6 +4,7 @@ import it.polimi.ingsw.common.events.Event;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class AsynchronousEventDispatcher extends EventDispatcher implements AutoCloseable {
     private final ExecutorService executor;
@@ -16,11 +17,26 @@ public class AsynchronousEventDispatcher extends EventDispatcher implements Auto
     public <T extends Event> void dispatch(T event) {
         if (executor.isShutdown())
             return;
-        executor.execute(() -> super.dispatch(event));
+        executor.submit(() -> super.dispatch(event));
+    }
+
+    public <T extends Event> void awaitDispatch(T event) {
+        super.dispatch(event);
     }
 
     @Override
     public void close() {
         executor.shutdown();
+    }
+
+    public void close(Runnable callback) {
+        if (executor.isShutdown())
+            return;
+        executor.shutdown();
+        try {
+            executor.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+        } catch (InterruptedException ignored) {
+        }
+        callback.run();
     }
 }
