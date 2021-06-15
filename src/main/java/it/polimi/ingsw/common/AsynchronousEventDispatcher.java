30d3b2e4e090944b932ecafd7c3287c3bcfc7d55
Damiano Amatruda <damiano.amatruda@outlook.com>
2021-06-15 18:53:42 +0200

    Refine AsynchronousEventDispatcher

--- src/main/java/it/polimi/ingsw/common/AsynchronousEventDispatcher.java
+++ src/main/java/it/polimi/ingsw/common/AsynchronousEventDispatcher.java
@@ -2,8 +2,7 @@ package it.polimi.ingsw.common;
 
 import it.polimi.ingsw.common.events.Event;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 
 public class AsynchronousEventDispatcher extends EventDispatcher implements AutoCloseable {
     private final ExecutorService executor;
@@ -16,11 +15,24 @@ public class AsynchronousEventDispatcher extends EventDispatcher implements Auto
     public <T extends Event> void dispatch(T event) {
         if (executor.isShutdown())
             return;
-        executor.submit(() -> super.dispatch(event));
+        asyncDispatch(event);
     }
 
     public <T extends Event> void awaitDispatch(T event) {
-        super.dispatch(event);
+        if (executor.isShutdown())
+            return;
+        try {
+            asyncDispatch(event).get();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        } catch (ExecutionException e) {
+            throw new RuntimeException(e);
+        } catch (CancellationException ignored) {
+        }
+    }
+
+    private <T extends Event> Future<?> asyncDispatch(T event) {
+        return executor.submit(() -> super.dispatch(event));
     }
 
     @Override
