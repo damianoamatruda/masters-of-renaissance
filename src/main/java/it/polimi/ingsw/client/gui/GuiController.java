95ccd19cab91edf25ba7ebe82201f7a50b4b8cad
Marco Ciarrocca <marco.ciarrocca@mekomi.dev>
2021-06-16 23:23:59 +0200

    Add setup/reconnection-sensing state switching method to GuiController

--- src/main/java/it/polimi/ingsw/client/gui/GuiController.java
+++ src/main/java/it/polimi/ingsw/client/gui/GuiController.java
@@ -17,6 +17,34 @@ public abstract class GuiController extends UiController implements Initializabl
         Gui.getInstance().setController(this);
     }
 
+    /**
+     * Sets the next state based on the following algorithm:
+     * 
+     * UpdateGame tells the client whether the game's setup phase is still ongoing or not.
+     * If the setup is done, the client needs to wait until UpdateCurrentPlayer to know which state to change to.
+     * If the setup phase is not done:
+     * The local player's UpdatePlayer tells the client what part of the player setup to switch to.
+     * If the leaders hand still needs to be chosen the client will need to wait for UpdateLeadersHand.
+     */
+    protected void setNextSetupState() {
+        vm.isSetupDone().ifPresent(isSetupDone -> { // received UpdateGame (if not, wait for it)
+            if (isSetupDone) { // setup is done
+                if (vm.getCurrentPlayer().equals(vm.getLocalPlayerNickname()))
+                    gui.setRoot(getClass().getResource("/assets/gui/playgroundbeforeaction.fxml"));
+                else
+                    gui.setRoot(getClass().getResource("/assets/gui/waitingforturn.fxml"));
+            } else // setup not done
+                vm.getPlayerData(vm.getLocalPlayerNickname()).ifPresent(pd -> {
+                    pd.getSetup().ifPresent(setup -> { // received local player's setup
+                        if (isLeaderSetupAvailable())
+                            gui.setRoot(getClass().getResource("/assets/gui/setupleaders.fxml"));
+                        else if (isResourceSetupAvailable())
+                            gui.setRoot(getClass().getResource("/assets/gui/setupresources.fxml"));
+                    });
+                });
+        });
+    }
+
     @FXML
     public void initialize(URL url, ResourceBundle resourceBundle) {
     }
