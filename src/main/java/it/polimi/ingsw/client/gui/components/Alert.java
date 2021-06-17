f16f488c47adf0b94f35eb8831ad5dfffb01f839
Marco Ciarrocca <marco.ciarrocca@mekomi.dev>
2021-06-17 19:26:23 +0200

    GUI: Refactor Alert to use FXML file and refine graphics

--- src/main/java/it/polimi/ingsw/client/gui/components/Alert.java
+++ src/main/java/it/polimi/ingsw/client/gui/components/Alert.java
@@ -1,49 +1,59 @@
 package it.polimi.ingsw.client.gui.components;
 
+import java.io.IOException;
+
 import javafx.beans.binding.NumberBinding;
 import javafx.event.ActionEvent;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
-import javafx.scene.control.Button;
+import javafx.scene.layout.Border;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.BorderStroke;
+import javafx.scene.layout.BorderStrokeStyle;
+import javafx.scene.layout.BorderWidths;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextAlignment;
 
 public class Alert extends BorderPane {
+    @FXML private VBox window;
+    @FXML private Title titleText;
+    @FXML private SButton okButton;
+    @FXML private Text contentText;
 
     public Alert() {}
 
     public Alert(String title, String content, NumberBinding sizeBinding) {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/assets/gui/components/alert.fxml"));
+        fxmlLoader.setRoot(this);
+        fxmlLoader.setController(this);
+
+        try {
+            fxmlLoader.load();
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }
+
         this.scaleXProperty().bind(sizeBinding);
         this.scaleYProperty().bind(sizeBinding);
 
-        Text titleText = new Text(title);
-        titleText.setWrappingWidth(600);
-        titleText.setTextAlignment(TextAlignment.CENTER);
-        titleText.setStyle("-fx-font-size: 40px;");
+        titleText.setText(title);
 
-        Text contentText = new Text(content);
-        contentText.setWrappingWidth(600);
+        contentText.setText(content);
+        contentText.setWrappingWidth(300);
         contentText.setTextAlignment(TextAlignment.CENTER);
 
-        VBox canvas = new VBox(10, titleText, contentText);
-        canvas.setAlignment(Pos.CENTER);
-
-        this.setCenter(canvas);
-
-        Button okButton = new SButton("OK");
         okButton.setDefaultButton(true);
         okButton.addEventHandler(ActionEvent.ACTION, (ActionEvent actionEvent) -> {
             ((Pane) getParent()).getChildren().remove(this);
         });
-        this.setBottom(okButton);
-        BorderPane.setAlignment(okButton, Pos.CENTER);
-        BorderPane.setMargin(okButton, new Insets(20));
 
-        this.getStyleClass().add("main");
+        window.setBorder(new Border(new BorderStroke(Color.rgb(214, 150, 0),
+            BorderStrokeStyle.SOLID, new CornerRadii(5), new BorderWidths(3))));
     }
 
     public Alert(Node center, Node top, Node right, Node left) {
