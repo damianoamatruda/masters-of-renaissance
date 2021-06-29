b733b8f273ceea2770ae1e2293c0309dc710282c
Marco Ciarrocca <marco.ciarrocca@mekomi.dev>
2021-06-30 01:07:24 +0200

    GUI: Fix WaitingforTurnController's state switching logic

--- src/main/java/it/polimi/ingsw/client/gui/WaitingForTurnController.java
+++ src/main/java/it/polimi/ingsw/client/gui/WaitingForTurnController.java
@@ -5,6 +5,10 @@ import javafx.scene.layout.AnchorPane;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import it.polimi.ingsw.common.events.mvevents.UpdateAction;
+import it.polimi.ingsw.common.events.mvevents.UpdateCurrentPlayer;
+import it.polimi.ingsw.common.events.mvevents.UpdateAction.ActionType;
+
 /**
  * Gui controller used when local player is not the current player. The current player's leaderboard is shown (without
  * their leader cards and/or other secret components).
@@ -22,5 +26,15 @@ public class WaitingForTurnController extends TurnController {
         canvas.getChildren().add(title);
 
         setLeadersBox(20d, 30d);
+    }
+
+    @Override
+    public void on(UpdateAction event) {
+        super.on(event);
+
+        if (event.getAction() == ActionType.END_TURN &&
+            !event.getPlayer().equals(vm.getLocalPlayerNickname()) &&
+            vm.getCurrentPlayer().equals(vm.getLocalPlayerNickname()))
+            setNextState();
     }
 }
