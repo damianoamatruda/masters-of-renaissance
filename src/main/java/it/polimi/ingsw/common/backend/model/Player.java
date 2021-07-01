1aff9ca4f7df0d22fbe20dbef074f6b89ac14d7b
Marco Ciarrocca <marco.ciarrocca@mekomi.dev>
2021-07-01 11:24:37 +0200

    Fix exception related to leaders hand being null

--- src/main/java/it/polimi/ingsw/common/backend/model/Player.java
+++ src/main/java/it/polimi/ingsw/common/backend/model/Player.java
@@ -356,7 +356,7 @@ public class Player extends EventDispatcher {
                 warehouse.getShelves().stream().map(ResourceContainer::getId).toList(),
                 strongbox.getId(),
                 setup.reduce(),
-                includeLeadersHand ? leaders.stream().map(LeaderCard::getId).toList() : null,
+                includeLeadersHand ? leaders.stream().map(LeaderCard::getId).toList() : new ArrayList<>(),
                 leaders.size(),
                 devSlots.stream().map(devSlot -> devSlot.stream().map(DevelopmentCard::getId).toList()).toList(),
                 faithPoints,
