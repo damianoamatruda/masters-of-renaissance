4fcba67c9755ca2344455fbccbb2e11f292b3bb5
Marco Ciarrocca <marco.ciarrocca@mekomi.dev>
2021-07-02 22:29:41 +0200

    CLI: Fix LeaderBoard

--- src/main/java/it/polimi/ingsw/client/cli/components/LeaderBoard.java
+++ src/main/java/it/polimi/ingsw/client/cli/components/LeaderBoard.java
@@ -3,18 +3,16 @@ package it.polimi.ingsw.client.cli.components;
 import it.polimi.ingsw.client.ViewModel;
 import it.polimi.ingsw.client.cli.Cli;
 
-import static it.polimi.ingsw.client.cli.Cli.center;
-
 public class LeaderBoard extends StringComponent {
     @Override
     public String getString() {
         ViewModel vm = Cli.getInstance().getViewModel();
 
-        StringBuilder stringBuilder = new StringBuilder(center("Leaderboard:"));
-        stringBuilder.append(center("Nickname\tFaith Points\tVictory points"));
+        StringBuilder stringBuilder = new StringBuilder("Leaderboard:");
+        stringBuilder.append("Nickname\tFaith Points\tVictory points");
 
         vm.getPlayers().forEach(p ->
-                stringBuilder.append(center(String.format("%s\t%d\t%d", p.getNickname(), p.getFaithPoints(), p.getVictoryPoints()))));
+                stringBuilder.append(String.format("%s\t%d\t%d", p.getNickname(), p.getFaithPoints(), p.getVictoryPoints())));
 
         return stringBuilder.toString();
     }
