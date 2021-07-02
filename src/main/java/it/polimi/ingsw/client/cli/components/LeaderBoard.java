225729a24322d58782ca93e0772f5789cef618d0
Marco Ciarrocca <marco.ciarrocca@mekomi.dev>
2021-07-02 23:32:14 +0200

    CLI: Refactor LeaderBoard

--- src/main/java/it/polimi/ingsw/client/cli/components/LeaderBoard.java
+++ src/main/java/it/polimi/ingsw/client/cli/components/LeaderBoard.java
@@ -3,20 +3,16 @@ package it.polimi.ingsw.client.cli.components;
 import it.polimi.ingsw.client.ViewModel;
 import it.polimi.ingsw.client.cli.Cli;
 
-import static it.polimi.ingsw.client.cli.Cli.left;
-
 public class LeaderBoard extends StringComponent {
     @Override
     public String getString() {
         ViewModel vm = Cli.getInstance().getViewModel();
 
         StringBuilder stringBuilder = new StringBuilder("Leaderboard:\n");
-        stringBuilder.append("Nickname").append(" ".repeat(5)).append("Faith Points").append(" ".repeat(5)).append("Victory points\n");
+        stringBuilder.append("Nickname - Faith Points - Victory points\n");
 
         vm.getPlayers().forEach(p -> {
-            stringBuilder.append(left(String.format("%s", p.getNickname()), 13));
-            stringBuilder.append(left(String.format("%d", p.getFaithPoints()), 17));
-            stringBuilder.append(left(String.format("%d\n", p.getVictoryPoints()), 19));
+            stringBuilder.append(String.format("%s - %d - %d%n", p.getNickname(), p.getFaithPoints(), p.getVictoryPoints()));
         });
 
         return stringBuilder.toString();
